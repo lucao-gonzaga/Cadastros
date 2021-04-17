d1ab09c59f09c95f89220c26346cb5e3bdc28177
Lucas <lucasbenda@outlook.com>
2021-04-16 22:25:16 -0300

    Adicionar arquivos de projeto.

---
+++ Cadastros/Controllers/CadastroController.cs
@@ -0,0 +1,48 @@
+﻿using Cadastros.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastros.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public static IList<Cadastro> cadastros = new List<Cadastro>()
+        {
+            new Cadastro()
+            {
+                CadastroID = 1,
+                Nome = "Lucas",
+                Livro = "nao sei qual nome",
+                Endereco = "Rua P, 225 - Campinas",
+                Email = "lucasbenda@outlook.com",
+                Telefone = "1231-4124",
+                Validade = "21/05/2021"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(cadastros);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        #region POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Cadastro cad)
+        {
+            cadastros.Add(cad);
+            cad.CadastroID = cadastros.Select(c => c.CadastroID).Max() + 1;
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+    }
+}
---
+++ Cadastros/Models/Cadastro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastros.Models
+{
+    public class Cadastro
+    {
+        [Key]
+        public long? CadastroID { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Display(Name = "Livro")]
+        public string Livro { get; set; }
+
+        [Display(Name = "Endereço")]
+        public string Endereco { get; set; }
+
+        [EmailAddress(ErrorMessage = "Email invalido")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+        public string Validade { get; set; }
+    }
+}
